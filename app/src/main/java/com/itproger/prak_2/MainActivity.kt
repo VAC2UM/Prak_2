5a8a7d65ac6dd08b7842185cb01a7a0f301f622a
VAC2UM <nikita.golowanev@gmail.com>
2024-09-12 22:43:23 +0300

    Initial commit

---
+++ app/src/main/java/com/itproger/prak_2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.itproger.prak_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val fragment1: Fragment1 = Fragment1()
+            val fragmentManager: FragmentManager = supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, fragment1)
+            fragmentTransaction.commit()
+        }
+    }
+}
